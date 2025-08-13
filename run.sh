d788f17afe69fb333467ee8de63f7a06dfbf06d9
streamer-os <m18145549929@163.com>
2025-08-13 19:31:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ export E2B_API_KEY=e2b_bd27fe62abed5c9baef572f60d75576a1362c914
 nohup python scripts/e2b_router.py > e2b_router.log 2>&1 &
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 ACCELERATE_LOG_LEVEL=info \
-    accelerate launch --config_file recipes/accelerate_configs/zero2.yaml --num_processes 4 \
+    accelerate launch --config_file recipes/accelerate_configs/zero2.yaml --num_processes 4 --main_process_port 29501 \
     src/open_r1/grpo.py --config recipes/config_coupled_code.yaml
