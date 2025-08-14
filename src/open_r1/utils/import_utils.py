53e938bbdd0e64482e06fabea6a25184f5adc9ef
streamer-os <m18145549929@163.com>
2025-08-14 21:09:44 +0800

    Update import_utils.py

--- src/open_r1/utils/import_utils.py
+++ src/open_r1/utils/import_utils.py
@@ -1,1 +1,16 @@
+from transformers.utils.import_utils import _is_package_available
 
+
+# Use same as transformers.utils.import_utils
+_e2b_available = _is_package_available("e2b")
+
+
+def is_e2b_available() -> bool:
+    return _e2b_available
+
+
+_morph_available = _is_package_available("morphcloud")
+
+
+def is_morph_available() -> bool:
+    return _morph_available
